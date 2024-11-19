47a8fcd99b375ecfef531c11a85f94d1cf3afb13
asif883 <asifiqbal95863@gmail.com>
2024-11-19 12:55:10 +0600

    verify jwt & verify seller

--- index.js
+++ index.js
@@ -7,12 +7,44 @@ const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200
+}))
 app.use(express.json())
+
+
+// token verification 
+const verifyJwt =(req, res , next) =>{
+  const authorization = req.header.authorization
+  if( !authorization ){
+    return res.send({ message: "No token found"})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token , process.env.ACCESS_TOKEN, (err, decoded) =>{
+    if(err){
+      return res.send({message : "Invalid token"})
+    }
+    req.decoded= decoded;
+    next()
+  })
+} 
+
+// verify Seller
+
+ const verifySeller = async ( req ,res ,next)=>{
+  const email = req.decoded.email
+  const query = { email : email}
+  const user = await userCollection.findOne(query)
+  if(user?.role !== 'seller'){
+    return res.send({message: "Forbidden access"})
+  }
+  next()
+ }
 
 // mongoDB
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osztyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.osztyuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;;
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -21,11 +53,37 @@ const client = new MongoClient(uri, {
       deprecationErrors: true,
     }
   });
+     
+  const userCollection = client.db("gadgetShop").collection('users');
+  const allProduct =client.db('gadgetShop').collection('products')
 
   const dbConnect =async ()=>{
     try{
       await  client.connect();
       console.log('DB connected')
+
+      // insert Users
+      app.post('/users', async( req, res )=>{
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result)
+      })
+
+      // get user
+      app.get('/user/:email' , async ( req, res )=>{
+        const query = {email: req.params.email}
+        const user = await userCollection.findOne(query)
+        res.send(user)    
+      })
+
+      // Add Product
+     app.post('/add-product',verifyJwt, verifySeller, async( req, res )=>{
+        const product = req.body
+        const result = await allProduct.insertOne(product)
+        res.send(result)
+     }) 
+
+
     }
     catch(error){
         console.log(error.name , error.message)
